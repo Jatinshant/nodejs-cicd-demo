47c2a0838bbce935f565ee1f20107d46725d6dea
jatinshant-lgtm <jatin.shant@resourcedekho.com>
2026-06-10 19:32:26 +0530

    edit app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200);
-    res.end('Congratulations Jatin CI/CD Pipeline Working fine');
+    res.end('Congratulations Jatin Shant CI/CD Pipeline Working fine');
 });
 
 server.listen(3000, () => {
